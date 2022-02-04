47928520adfaa79fe014ad9dbe1fbfb8df910bde
Kenneth Radoor <kenneth@imade.dk>
2022-02-04 01:28:45 +0100

    Added Models for Rounds and Scores

---
+++ ScoreKeeper.Core/Models/UserHoleScore.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScoreKeeper.Core.Models
+{
+    public class UserHoleScore
+    {
+        public Guid UserHoleScoreId { get; set; }
+        public Guid UserId { get; set; }
+        public Guid CourseHoleId { get; set; }
+        public int Strokes { get; set; }
+
+    }
+}
---
+++ ScoreKeeper.Core/Models/UserRound.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScoreKeeper.Core.Models
+{
+    public class UserRound
+    {
+        public Guid UserRoundId { get; set; }
+        public Guid UserId { get; set; }
+        public Guid CourseId { get; set; }
+        public double UserHcp { get; set; }
+        public DateTime PlayedAt { get; set; } = DateTime.Now;
+    }
+}
